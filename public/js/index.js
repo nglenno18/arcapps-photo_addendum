ccfa8911dd695bacab2300d034fd405a88093feb
nglenno18 <nglenno18@gmail.com>
2017-02-12 13:31:25 -0600

    Add, delete rows, multi-select starts from first empty box

--- public/js/index.js
+++ public/js/index.js
@@ -112,14 +112,20 @@ socket.on('connect', function(){
 
         var files = evt.currentTarget.files;
         var length = event.currentTarget.files.length;
-        for(i = 0; i <(length-2)/2; i++){
+
+        for(i = 0; i <(length)/2; i++){
           console.log('\n\n\n\n\nADD ROWS');
+          ///read if there are any existing empy rows
           $('#add-row').click();
         }
+        if(nextBox === ""){
+          console.log('\n\n\nBOXES FILLED: ', filledBoxes);
+          nextBox = "box" + (filledBoxes.length+1);
+          console.log('\n\n\nNEXT BOX: ', nextBox);
+        }
 
-        // console.log('PREVIOUS BOX ID: ', prevBoxid);
 
-        // selectBox(selectedBox);
+
         var boxnum = nextBox.substring(nextBox.lastIndexOf('x')+1);
         // console.log('BOX TO INPUT TO: #', boxnum);
         // console.log('# of files: ', length);
@@ -128,7 +134,7 @@ socket.on('connect', function(){
 
         var index = parseInt(boxnum)-1;
         // var index = 0;
-        for(x = 0; x <=length -1; x++){
+        for(x = 0; x <= length -1; x++){
           var i = event.currentTarget.files[x];
           // console.log(i);
           i = URL.createObjectURL(i);
@@ -148,7 +154,6 @@ socket.on('connect', function(){
           var h;
           var w;
           $('#box'+index).html('<img class="img" src="' + i + '"/>');
-
         }
       });
     });
@@ -178,14 +183,35 @@ socket.on('connect', function(){
     }
   });
 
+  $('#delete-row').on('click', function(i){
+    console.log('DELETE Row!!', w);
+    var au = document.getElementById('au' + w);
+    console.log(au);
+    au.parentNode.removeChild(au);
+    w-=2;
+    if(w < 3) return $('#delete-row').css('display', 'none');
+  });
   $('#add-row').on('click', function(i){
     w+=2;
+    if(w >=3) $('#delete-row img').css('display', 'inline-block');
     console.log('ADD a new Row!!', w);
     var n = document.getElementById('au'); //returns a HTML DOM Object
     // console.log(n);
     var dupNode = n.cloneNode(true);
     console.log(dupNode);
-    console.log(dupNode.children[0].children);
+    var au = dupNode.children[0].children;
+    console.log('\n\nAU: ', au);
+
+    var textboxes = dupNode.getElementsByTagName('input');
+    for(x = 0; x <= textboxes.length; x++){
+      if(textboxes[x]){
+        if(textboxes[x].getAttribute("type")){
+          textboxes[x].value = "";
+        }
+      }
+    }
+    console.log('TEXT DESCs: ', textboxes);
+
     var b1 = dupNode.children[0].children[0].children[0];
     var b2 = dupNode.children[0].children[2].children[0];
     var sp1 = dupNode.children[0].children[1];
@@ -205,21 +231,19 @@ socket.on('connect', function(){
     if(b1img){
       b1img.src = 'images/temp2.png';
     }
-    // $('#'+b1.id).css("margin-left", "20px");
-    // $('#'+b1.id).css("float", "left");
-    // $('#'+b1.id +' img').css("width", "90% !important");
-    //
-    //
-    // console.log('BOX1: ', b1);
-    //
+
+
     b2.id = "box"+ (w+1);
+    var b2img = b2.getElementsByClassName('img')[0];
+    console.log('IMAGE: ', b2img);
+    if(b2img){
+      b2img.src = 'images/temp2.png';
+    }
     // $('#'+b2.id).css("margin-right", "20px");
     // $('#'+b2.id).css("float", "right");
     // $('#'+b2.id).css("width", "300px");
     // $('#'+b2.id).css("border", "2px black solid");
-    // console.log('BOX2: ', b2);
 
-    // dupNode.attr("box1","box"+w);
     var doc = document.getElementById("page-container");
     doc.appendChild(dupNode);
     console.log(dupNode);
